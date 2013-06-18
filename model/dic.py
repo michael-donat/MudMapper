5bb0b823a2df48e48bfd503beadd99aa0702021d
Michael Donat <thornag@gmail.com>
2013-06-18 10:40:30 +0100

    fix build #8191327

---
+++ model/dic.py
@@ -0,0 +1,69 @@
+
+from controller import mainWindow as mainWindowController, mapViewport as mapViewportController, map as mapController
+import model.map as mapModel
+import model.helper as helperModel
+import view.map as viewMap
+
+
+class Container:
+    __config = None;
+    __objects=None
+    def __init__(self, config):
+        self.__config = config
+        self.__objects={}
+
+
+    def getMainWindowFileMenuController(self):
+        if not self.__objects.has_key('controller.mainwindow.filemenu'):
+            self.__objects['controller.mainwindow.filemenu'] = mainWindowController.FileMenu()
+
+        return self.__objects['controller.mainwindow.filemenu']
+
+    def getMainWindowToolbarController(self):
+        if not self.__objects.has_key('controller.mainwindow.toolbar'):
+            self.__objects['controller.mainwindow.toolbar'] = toolbar = mainWindowController.Toolbar()
+            toolbar.setConfig(self.__config)
+            toolbar.setViewportController(self.getMapViewportController())
+
+        return self.__objects['controller.mainwindow.toolbar']
+
+    def getMapController(self):
+        if not self.__objects.has_key('controller.map'):
+            self.__objects['controller.map'] = mapControllerObject = mapController.Map()
+            mapControllerObject.setFactory(self.getMapFactory())
+            mapControllerObject.setGeometryHelper(self.getHelperGeometry())
+
+        return self.__objects['controller.map']
+
+    def getMapViewportController(self):
+        if not self.__objects.has_key('controller.map.viewport'):
+            self.__objects['controller.map.viewport'] = mapViewportControllerObject = mapViewportController.Map()
+            mapViewportControllerObject.setRoomViewRoutine(self.getViewRoom)
+
+        return self.__objects['controller.map.viewport']
+
+    def getMapFactory(self):
+        if not self.__objects.has_key('factory.map'):
+            self.__objects['factory.map'] = factoryObject = mapModel.Factory()
+            factoryObject.setConfig(self.__config.drawing())
+
+        return self.__objects['factory.map']
+
+    def getViewRoom(self):
+        newRoom = viewMap.Room()
+        newRoom.setGeometryHelper(self.getHelperGeometry())
+        newRoom.setRoomComponents(self.getViewRoomComponents())
+
+        return newRoom
+
+    def getHelperGeometry(self):
+        if not self.__objects.has_key('helper.geometry'):
+            self.__objects['helper.geometry'] = helperModel.Geometry(self.__config.drawing())
+
+        return self.__objects['helper.geometry']
+
+    def getViewRoomComponents(self):
+        if not self.__objects.has_key('view.map.roomComponents'):
+            self.__objects['view.map.roomComponents'] = viewMap.RoomComponents(self.__config.drawing())
+
+        return self.__objects['view.map.roomComponents']
